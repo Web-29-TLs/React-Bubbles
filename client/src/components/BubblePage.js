bbb7cc8ab3c599529454b23e03be3286624948a8
Schrese <53787633+Schrese@users.noreply.github.com>
2019-12-20 11:52:12 -0500

    fetched bubbles in BubblePage.js

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import {axiosWithAuth} from '../utils/axiosWithAuth';
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
@@ -8,6 +8,20 @@ const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+
+  //fetches bubbles from backend 
+  useEffect(() => {
+    axiosWithAuth()
+      .get('/colors')
+      .then(res => {
+        console.log('res from BubblePage', res)
+        setColorList(res.data)
+      })
+      .catch(err => {
+        console.log('err from BubblePage', err)
+      })
+  }, [])
+
 
   return (
     <>
